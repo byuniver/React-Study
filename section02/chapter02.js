dff496138e59d63211ee12e0c1ada1bef1f43097
변경민 <byuniver@gmail.com>
2025-05-15 22:23:28 +0900

    단락 평가가

---
+++ section02/chapter02.js
@@ -0,0 +1,24 @@
+// function returnFalse() {
+//   console.log("False 함수");
+//   return false;
+// }
+
+// function returnTrue() {
+//   console.log("True 함수");
+//   return true;
+// }
+
+// console.log(returnTrue() && returnFalse());
+
+// 단락 평가 활용 사례
+function printName(person) {
+  //   if (!person) {
+  //     console.log("person에 값이 없음");
+  //     return;
+  //   } 아래와 같이 간략하게 가능
+  const name = person && person.name;
+  console.log(name || "person의 값이 없음");
+}
+
+printName();
+printName({ name: "홍길동" });
