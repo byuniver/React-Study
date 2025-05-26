9c1c956e31512754ef9b08e800f9e0ddfc89008d
변경민 <byuniver@gmail.com>
2025-05-26 20:09:46 +0900

    React Study

---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// function header() {를
+// const Header = () => {} 같은 방식으로 선언 O
+// 첫 글자는 대문자여야함. 그래야 컴포넌트로 인식
+const Header = () => {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
+export default Header;
