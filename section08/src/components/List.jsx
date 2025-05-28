2f18b833a157ef7b28868a3e37b564ef049b7c6b
변경민 <byuniver@gmail.com>
2025-05-28 22:43:02 +0900

    React Study

---
+++ section08/src/components/List.jsx
@@ -0,0 +1,17 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+const List = ({ todos }) => {
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        {todos.map((todo) => {
+          return <TodoItem {...todo} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
